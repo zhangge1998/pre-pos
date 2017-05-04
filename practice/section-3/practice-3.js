6e2031d952a93b90d8ee1d4a982fdd1800027abb
zhangge1998 <1263061442@qq.com>
2017-05-04 22:13:07 +0800

    functional programming

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,32 +1,35 @@
 'use strict';
 
-function createUpdatedCollection(collectionA, objectB) {
-  var arr_result=[];
-  var i=0;
-  while(i<collectionA.length)
-  {
-    var count=0;
-    var temp=collectionA[i];
-    for(var j=i;j<collectionA.length;j++)
-    {
-      if(collectionA[j]==temp)
-      {
-        count+=1;
+function getCountSameElements(collectionA) {
+  var arr_result = [];
+  var i = 0;
+  while (i < collectionA.length) {
+    var count = 0;
+    var temp = collectionA[i];
+    for (var j = i; j < collectionA.length; j++) {
+      if (collectionA[j] == temp) {
+        count += 1;
         i = j + 1;
       }
     }
-    arr_result.push({key:temp,count:count});
-  }
-  for(var k=0;k<arr_result.length;k++)
-  {
-    for(var c=0;c<objectB.value.length;c++)
-    {
-      if(arr_result[k].key==objectB.value[c])
-      {
-        if(arr_result[k].count>=3)
-          arr_result[k].count-=parseInt(arr_result[k].count/3);
-      }
-    }
+    arr_result.push({key: temp, count: count});
   }
   return arr_result;
+}
+function createUpdatedCollection(collectionA, objectB) {
+  var result = getCountSameElements(collectionA);
+  for (var i = 0; i < result.length; i++) {
+    if (isExit(result[i], objectB))
+      isLargeThanThree(result[i]);
+  }
+  return result;
+}
+function isExit(a, objectB) {
+  for (var j = 0; j < objectB.value.length; j++)
+    if (a.key == objectB.value[j])
+      return 1;
+}
+function isLargeThanThree(a) {
+  if (a.count >= 3)
+    a.count -= parseInt(a.count / 3);
 }
