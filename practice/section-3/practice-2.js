ae988a72ef675572d56bb8daa8225afd38b31e35
zhangge1998 <1263061442@qq.com>
2017-05-08 20:12:41 +0800

    modify the parameter name and simplify the function name

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -3,18 +3,18 @@
 function createUpdatedCollection(collectionA, objectB) {
   for (var i = 0; i < collectionA.length; i++) {
     if (isExit(collectionA[i], objectB))
-      isLargeThanThree(collectionA[i]);
+      isLarge(collectionA[i]);
   }
   return collectionA;
 }
 
-function isExit(a, objectB) {
+function isExit(element, objectB) {
   for (var j = 0; j < objectB.value.length; j++)
-    if (a.key == objectB.value[j])
+    if (element.key == objectB.value[j])
       return 1;
 }
 
-function isLargeThanThree(a) {
-  if (a.count >= 3)
-    a.count -= parseInt(a.count / 3);
+function isLarge(element) {
+  if (element.count >= 3)
+    element.count -= parseInt(element.count / 3);
 }
