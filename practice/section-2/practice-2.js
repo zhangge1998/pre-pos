45dcf9f60f30d4e003fcfd65d6fceb90667396bf
zhangge1998 <1263061442@qq.com>
2017-05-08 20:06:38 +0800

    use word 'subscript'

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,14 +1,14 @@
 'use strict';
 function countSameElements(collection) {
   var elementCounts = [{key: 'a', count: 0}];
-  var t = 0;
+  var subscript = 0;
   for (var j = 0; j < collection.length; j++)
     if (collection[j].length == 1)
-      if (elementCounts[t].key == collection[j])
-        elementCounts[t].count += 1;
+      if (elementCounts[subscript].key == collection[j])
+        elementCounts[subscript].count += 1;
       else {
-        t = t + 1;
-        elementCounts[t] = {key: collection[j], count: 1};
+        subscript = subscript + 1;
+        elementCounts[subscript] = {key: collection[j], count: 1};
            }
     else
       elementCounts.push({key: collection[j].substring(0, 1), count: parseInt(collection[j].substring(2, 3))});
