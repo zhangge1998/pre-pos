1dc84fd7ed899d13306f1b01afad1c2f089db7ab
zhangge1998 <1263061442@qq.com>
2017-05-08 19:44:41 +0800

    simplify the function  names

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -3,13 +3,13 @@
 function collectSameElements(collectionA, collectionB) {
   var sameElements = [];
   for (var i = 0; i < collectionA.length; i++) {
-    if (isExitSameElement(collectionA[i], collectionB))
+    if (isExit(collectionA[i], collectionB))
       sameElements.push(collectionA[i]);
   }
   return sameElements;
 }
 
-function isExitSameElement(element, collectionB) {
+function isExit(element, collectionB) {
   for (var j = 0; j < collectionB.length; j++)
     if (element == collectionB[j])
       return 1;
