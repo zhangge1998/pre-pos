2435380d12d9fae1e7a53955bf62709c58a9391f
zhangge1998 <1263061442@qq.com>
2017-05-08 20:16:41 +0800

    modify the parameter name and simplify the function name and use 'subscript'

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -3,31 +3,31 @@ function createUpdatedCollection(collectionA, objectB) {
   var result = getCountSameElements(collectionA);
   for (var i = 0; i < result.length; i++) {
     if (isExit(result[i], objectB))
-      isLargeThanThree(result[i]);
+      isLarge(result[i]);
   }
   return result;
 }
 
 function getCountSameElements(collectionA) {
   var elementCounts = [{key: 'a', count: 0}];
-  var t = 0;
+  var subscript = 0;
   for (var i = 0; i < collectionA.length; i++) {
-    if (elementCounts[t].key == collectionA[i])
-      elementCounts[t].count += 1;
+    if (elementCounts[subscript].key == collectionA[i])
+      elementCounts[subscript].count += 1;
     else {
-      t = t + 1;
-      elementCounts[t] = {key: collectionA[i], count: 1};
+      subscript = subscript + 1;
+      elementCounts[subscript] = {key: collectionA[i], count: 1};
     }
   }
   return elementCounts;
 }
 
-function isExit(a, objectB) {
+function isExit(element, objectB) {
   for (var j = 0; j < objectB.value.length; j++)
-    if (a.key == objectB.value[j])
+    if (element.key == objectB.value[j])
       return 1;
 }
-function isLargeThanThree(a) {
-  if (a.count >= 3)
-    a.count -= parseInt(a.count / 3);
+function isLarge(element) {
+  if (element.count >= 3)
+    element.count -= parseInt(element.count / 3);
 }
